9338512fc01a67244cd253ca6a8e0ded62bb7860
André Samuelsson <andresamuelsson94@gmail.com>
2018-03-12 18:38:15 +0100

    Remove image from lock screen

--- bin/mylock.sh
+++ bin/mylock.sh
@@ -3,5 +3,5 @@ ICON=$HOME/dotfiles/middle_finger.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+#convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 i3lock -u -i $TMPBG
